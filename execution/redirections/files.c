1b6a075d429fd7b8bd9c2323dc7f504d7ec144c1
NOUHAILA ERRAKEB <116765284+nouhaerr@users.noreply.github.com>
2023-06-11 05:02:17 +0100

    should duplicate fd

--- execution/redirections/files.c
+++ execution/redirections/files.c
@@ -12,23 +12,54 @@
 
 #include "../../includes/minishell.h"
 
-int	ft_open(t_parser *node)
+int	ft_open(char *name, char *msg)
 {
-    int			fd;
-	t_parser	*cur;
+	int	fd;
 
+	if (!ft_strcmp(msg, "saved_file"))
+		fd = open(name, O_CREAT | O_WRONLY | O_APPEND, 0644);
+	else if (!ft_strcmp(msg, "outfile"))
+		fd = open(name, O_CREAT | O_WRONLY | O_TRUNC, 0644);
+	else
+		fd = open(name, O_CREAT | O_RDONLY, 0644);
+	if (fd == -1)
+	{
+		perror(name);
+		if (pid != 0) //ila kena fl parent
+			exit(1);
+		else
+			fd = -3;
+	}
+	return (fd);
+}
+
+int	fd_redirection(t_parser *node)
+{
+    int			*fd;
+	t_parser	*cur;
+	t_data2		*tmp1;
+	t_data		*tmp2;
+
+	fd = malloc(sizeof(int) * 2);
 	cur = node;
+	tmp1 = node->outfiles;
+	tmp2 = node->infiles;
 	while(cur)
 	{
-		if (cur->outfiles)
+		while (tmp1)
 		{
-			if (cur->outfiles->type == finish_up)
-				fd = open(cur->outfiles->value, O_CREAT | O_WRONLY | O_APPEND, 0644);
-			else if (cur->outfiles->type == clear)
-				fd = open(cur->outfiles->value, O_CREAT | O_WRONLY | O_TRUNC, 0644);
+			if (tmp1->type == finish_up)
+				fd[1] = ft_open(tmp1->value, "saved_file");
+			else if (tmp1->type == clear)
+				fd[1] = ft_open(tmp1->value, "outfile");
+			if (fd[1] == -3)
+				return (free(fd), NULL);
+			tmp1 = tmp1->next;
 		}
-		else if (cur->infiles)
-			fd = open(cur->infiles->value, O_RDONLY, 0644);
-		cur = cur->next
+		while (tmp2)
+			fd[0] = ft_open(tmp2->value, "infile");
+			tmp2 = tmp2->next;
+		cur = cur->next;
 	}
+	return (fd);
 }
