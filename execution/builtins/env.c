84ba8850bbd48250d6b3f97352e9dbe1439fd9c9
Nouhaila Errakeb <nerrakeb@e2r5p11.1337.ma>
2023-06-26 00:06:22 +0100

    ok

--- execution/builtins/env.c
+++ execution/builtins/env.c
@@ -6,37 +6,37 @@
 /*   By: nerrakeb <nerrakeb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/23 04:57:29 by nerrakeb          #+#    #+#             */
-/*   Updated: 2023/06/17 11:29:20 by nerrakeb         ###   ########.fr       */
+/*   Updated: 2023/06/26 00:03:16 by nerrakeb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
-static void	path_not_found(void)
-{
-	ft_putstr_fd("minishell: env: No such file or directory\n", 2);
-	g_var.exit_status = 127;
-	return ;
-}
+// static void	path_not_found(void)
+// {
+// 	ft_putstr_fd("minishell: env: No such file or directory\n", 2);
+// 	g_var.exit_status = 127;
+// 	return ;
+// }
 
 void	sh_env(void)
 {
-	int		path_found;
+	// int		path_found;
 	t_env	*cur;
 
 	cur = g_var.list;
-	path_found = 0;
-	while (cur)
-	{
-		if (!ft_strcmp(cur->env, "PATH"))
-		{
-			path_found = 1;
-			break ;
-		}
-		cur = cur->next;
-	}
-	if (!path_found)
-		return (path_not_found());
+	// path_found = 0;
+	// while (cur)
+	// {
+	// 	if (!ft_strcmp(cur->env, "PATH"))
+	// 	{
+	// 		path_found = 1;
+	// 		break ;
+	// 	}
+	// 	cur = cur->next;
+	// }
+	// if (!path_found)
+	// 	return (path_not_found());
 	cur = g_var.list;
 	while (cur)
 	{
